bf29edb49784a37ae7ca562c46b7827d05441948
ayush <ayushdattarigal@gmail.com>
2022-08-12 13:06:49 +0100

    noraml object tips thst just takes in name days and percentage

--- functions.py
+++ functions.py
@@ -4,3 +4,13 @@ def hellooworld():
 hellooworld()
 
 # k lekhya muji ali ramro function lekh vaney hain mailaey
+
+class tips:
+    def __init__(self,name,day,percentage):
+        self.name= name
+        self.day= day
+        self.percentage= percentage
+    def get_name(self):
+        return self.day
+t=tips("ayush",["mon","tue"],10)
+print(t.get_name())
